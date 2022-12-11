5ae74ab28f5f2bcc050640f68a77d09da6977bb2
Mustafa MUEZZINOGLU <mustafamuezzinoglu@gmail.com>
2022-12-11 13:37:47 +0300

    added more hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing helleo feature");
+        System.out.println("Locak Repo is already published to GitHub");
+        System.out.println("Doing more work");
     }
 }
